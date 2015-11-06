f27c3cc4d0c6adc3ddbe943c603affa989464d2c
yangy2 <yangy2@tcnj.edu>
2015-11-06 18:56:53 -0500

    Create main file

---
+++ main/main.py
@@ -0,0 +1,25 @@
+Enter file contents here##Name: Yilin Yang
+##Course: CSC 415
+##Semester: Fall 2015
+##Instructor: Dr. Pulimood 
+##Project name: Assignment 5 - OSS Project: RoboPython
+##Description: Application teaches user how to code a NAO robot in Python
+##Filename: Setup.py
+##Description: File explains application and chooses which lesson to execute
+##Last modified on: 11/6/2015
+
+
+##Importing modules from the Naoqi OS
+from naoqi import ALProxy
+from naoqi import ALBroker
+from naoqi import ALModule
+
+##Import Setup file
+import Setup
+
+class Main():
+    ##Print welcome and instruction to user
+    print "Hey! Welcome to RoboPython!"
+    print "Enter 'Ctrl + C' in terminal to close application."
+    ##Setup application
+    Setup.initialize(ALModule)
